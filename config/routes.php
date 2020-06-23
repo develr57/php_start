7446a690d35cfd0bb4c6c471c302b37bdecfa0d7
develr57 <47880662+develr57@users.noreply.github.com>
2020-06-24 01:08:41 +0500

    реализация php фреймворка
    Создан класс Роутер, прописаны роуты. Таже создано 2 контроллера

---
+++ config/routes.php
@@ -0,0 +1,7 @@
+<?php
+
+return array(
+    'news'          => 'news/index',
+    'products'      => 'product/list',
+    'news/archive'  => 'news/archive',
+);
---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+
+//FRONT Controller
+
+// 1. Общие настройки
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+// 2. Подключение файлов системы
+define('ROOT', dirname(__FILE__));
+require_once(ROOT.'/components/Router.php');
+
+// 3. Установка соединения с БД
+
+
+
+// 4. Вызов Router
+$router = new Router();
+$router->run();
+
+
+// 5. My function
+// function debug($var)
+// {
+//     echo "<pre>";
+//     print_r($var);
+//     echo "</pre>";
+// }
